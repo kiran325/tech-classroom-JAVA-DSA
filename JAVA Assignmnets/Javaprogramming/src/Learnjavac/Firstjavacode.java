f110201b34c01e48781003ca862f23eb8da6c855
kiran325 <65232659+kiran325@users.noreply.github.com>
2025-08-17 00:01:23 +0530

    Basic java program

---
+++ JAVA Assignmnets/Javaprogramming/src/Learnjavac/Firstjavacode.java
@@ -0,0 +1,11 @@
+package Learnjavac;
+
+public class Firstjavacode {
+
+	public static void main(String[] args) {
+		
+		System.out.println("JAVA is Simple: to make it simple learn DSA");
+
+	}
+
+}
