5a4c34006397c2a3e1f8e5e0b4912f92d607cede
kiran325 <65232659+kiran325@users.noreply.github.com>
2025-10-03 00:12:14 +0530

    Java Basics Recent chnages

---
+++ Assignments/Javaprogramming/src/ExecuteJava/TakingMultipleValuefromkeyboard.java
@@ -0,0 +1,19 @@
+package ExecuteJava;
+
+import java.util.Scanner;
+
+public class TakingMultipleValuefromkeyboard {
+
+	public static void main(String[] args) {
+		
+		// Taking Multiple input Values from the keyboard 
+		
+		Scanner sc = new Scanner(System.in);
+		System.err.println("Please enter your String input: ");
+		String input = sc.next();
+		System.err.println("Please enter your Integer input: ");
+		int input1 = sc.nextInt();
+		
+	}
+
+}
