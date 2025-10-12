2ad1befaa5736f79425d535f3651805821069ea1
kiran325 <65232659+kiran325@users.noreply.github.com>
2025-10-12 23:57:25 +0530

    JAVA Basic recent changes

--- Assignments/Javaprogramming/src/ExecuteJava/EqualsToCheck.java
+++ Assignments/Javaprogramming/src/ExecuteJava/EqualsToCheck.java
@@ -23,12 +23,39 @@ public class EqualsToCheck {
         // Primitives don’t use .equals() (only objects do).
 		
 		String s1 = "welcome";
-		String s2 = "welcome";
+		String s2 = "Welcome";
 		System.out.println(s1==s2);
 		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(st2));
 		
 		// ✅ Both are true because Java interns string literals, meaning "Test" points to the same memory in the String Pool.
 
+		String str1 = "HelloWorld";
+		String str2 = "Welcome";
+		System.out.println("Length of String: "+ str1.length());
+		System.out.println("HelloWorld".length());
+		System.out.println(str1.concat(str2));
+		System.out.println("Welcome".concat(str2));
+		
+		String s3 = "   Welcome   ";
+		System.out.println(s3.trim());
+		
+		String s4 = "JAVA String";
+		System.out.println(s4.charAt(0)); // return character from string based on index value
+		
+		System.out.println(s4.contains("J")); // contains characters or string 
+		System.out.println(s4.replace('A', 'J')); // replace single/multiple(sequence) of characters in a string
+		System.out.println(s4.substring(7,11)); // substring extract substring from the main string 
+		
+		System.out.println(s4.toUpperCase()); // WELCOME
+		System.out.println(s4.toLowerCase()); // welcome
+		
+		String s = "abc@gmail.com";  // split() -split the string into multiple parts based on delimeter
+		String a1[]= s.split("@");
+		System.out.println(a1[0]);
+		System.out.println(a1[1]);
+		
+		
 	}
 
 }
