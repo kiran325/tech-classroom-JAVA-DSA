7718c7e5c358df8c5484ebaf00f35beaa7a13970
kiran325 <65232659+kiran325@users.noreply.github.com>
2025-12-04 12:50:28 +0530

    Java Basics Practice

--- Assignments/Javaprogramming/src/ExecuteJava/AgeValidation.java
+++ Assignments/Javaprogramming/src/ExecuteJava/AgeValidation.java
@@ -5,6 +5,13 @@ public class AgeValidation {
 	public static void main(String[] args) {
 		// Validate a person age is above 18, eligible for vote else not eligible 
 		
+//		int a[][] = new int [0][0];
+//		
+//		a[0][0]= 100;
+//		a[0][1]= 200;
+//		
+//		System.out.println(a[0][1]);
+		
 		int person_age = 20;
 		if(person_age>18) {
 			System.out.println("Person is eligible for vote");
