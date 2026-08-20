687da7769e84d8ad38ce023a6f95dd65b03025d6
kiran325 <65232659+kiran325@users.noreply.github.com>
2026-08-20 19:47:00 +0530

    Practice old Framework for better implemention

--- Automation Framework/src/test/java/NewFramework/DataConvertionMethods.java
+++ Automation Framework/src/test/java/NewFramework/DataConvertionMethods.java
@@ -4,7 +4,15 @@ public class DataConvertionMethods {
 
 	public static void main(String[] args) {
 		
+		//String s= "welcome"; // can't convert into int 
 		
+		String d="12345";
+		int sint = Integer.parseInt(d);
+		System.out.println(sint);
+		
+		String s1 = "10";
+		String s2 = "20";
+		System.out.println(Integer.parseInt(s1)+Integer.parseInt(s2));
 
 	}
 
